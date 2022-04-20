2f91daee7603d2f98cc89327ed4027df5a038a8e
thissideaditya <adityac486@gmail.com>
2022-04-20 15:14:38 +0530

    :sparkles: Login automation done

--- hackerrank.js
+++ hackerrank.js
@@ -1,13 +1,51 @@
 const puppeteer = require("puppeteer")
 
+let email = "adityac486@gmail.com"
+let password = "@dityaAD7"
+let cTab
 let browserOpenPromise = puppeteer.launch({
     headless:false,
     defaultViewport:null,
-    args: ["--start-maximized"]
+    args: ["--start-maximized"],
+    // chrome://version/ -> write in chrome to get the executablePath of chrome
+    // executablePath:"//Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
 })
 
 browserOpenPromise
-   .then(function () {
+   .then(function (browser) {
        console.log("browser is open")
+
+       // pages() -> An array of all open pages inside the Browser
+       let allTabsPromise = browser.pages()
+       return allTabsPromise
+   })
+   .then(function(allTabsArr){
+       cTab = allTabsArr[0]
+       console.log("New Tab opened")
+
+       // goto("url") -> opens given url
+       let visitingLoginPagePromise = cTab.goto("https://www.hackerrank.com/login")
+       return visitingLoginPagePromise
+   })
+   .then(function(){
+       console.log("HackerRank Login Page opened")
+       let emailTypePromise = cTab.type("input[name='username']", email)
+       return emailTypePromise
+   })
+   .then(function(){
+       console.log("email is entered")
+       let passTypePromise = cTab.type("input[name='password']", password)
+       return passTypePromise
+   })
+   .then(function(){
+       console.log("Password is entered")
+       let loggedInPromise = cTab.click(".ui-btn.ui-btn-large.ui-btn-primary.auth-button.ui-btn-styled")
+       return loggedInPromise
+   })
+   .then(function(){
+       console.log("Logged into hackerrank succesfully")
+   })
+   .catch(function(err){
+       console.log(err);
    })
 
